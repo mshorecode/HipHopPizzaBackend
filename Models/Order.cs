541b3c36dd626fd9589cc3eba32c2eb2ba4612a9
Ryan Shore <mshorecode@gmail.com>
2024-04-09 22:56:16 +0000

    setup Models folder

---
+++ Models/Order.cs
@@ -0,0 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace HipHopPizza.Models;
+
+public class Order
+{
+    public int Id { get; set; }
+    public string CustomerName { get; set; }
+    public string CustomerEmail { get; set; }
+    public int CustomerPhone { get; set; }
+    public int OrderTypeId { get; set; }
+    public decimal Subtotal { get; set; }
+    public decimal Tip { get; set; }
+    public decimal Total { get; set; }
+    public DateTime OrderDate { get; set; }
+    public bool IsComplete { get; set; }
+}
+
---
+++ Models/OrderType.cs
@@ -0,0 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace HipHopPizza.Models;
+
+public class OrderType
+{
+    public int Id { get; set; }
+    public string Type { get; set; }
+}
+
